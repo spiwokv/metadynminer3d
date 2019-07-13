575559ff69256e0b562cac5ecc0335e8305a5f73
Vojtech Spiwok <spiwokv@vscht.cz>
2019-07-13 18:47:08 +0200

    added references to metadynminer

--- R/readingandfes.R
+++ R/readingandfes.R
@@ -1,4 +1,5 @@
 #' @useDynLib metadynminer3d
+#' importFrom metadynminer feprof fesfes2 fesminima oneminimum plotheights
 #' @importFrom Rcpp sourceCpp
 #' @importFrom misc3d contour3d
 #' @importFrom rgl axes3d title3d box3d text3d points3d
